2e40a38de1f123f48c01da9b48373c5c5289b054
rahuljonwal247 <77151653+rahuljonwal247@users.noreply.github.com>
2024-12-08 20:33:47 +0530

    Update database.js
    database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize('postgresql://query_user:Pu6QYPVQzQCHz0x6sxGCBy9ULZsJgIUz@dpg-ctaqrbpu0jms73f270c0-a.oregon-postgres.render.com/query', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Ensure SSL is required
+      rejectUnauthorized: false, // Set to false to allow self-signed certificates
+    },
+  },
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
